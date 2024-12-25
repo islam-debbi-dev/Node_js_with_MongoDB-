8d97343eb82dc38e7c21b02b88d217a89605237d
islam_debbi <islamdebbi6@gmail.com>
2024-12-25 11:07:13 +0100

    delete phone from user  shema

--- models/users.js
+++ models/users.js
@@ -1,20 +1,19 @@
 const { name } = require('ejs');
 const mongoose = require('mongoose');
-const  Schema  = mongoose.Schema;
+const Schema = mongoose.Schema;
 const usersSchema = new Schema({
-            name: String,
-            age: Number,
-            email: String,
-            password: String,
-            Date: {
-                type: Date,
-                default: Date.now
-            },
-            sex: {
-                type: Boolean,
-                default: true,
-            },
-
+    name: String,
+    age: Number,
+    email: String,
+    password: String,
+    Date: {
+        type: Date,
+        default: Date.now
+    },
+    sex: {
+        type: Boolean,
+        default: true,
+    },
 });
 
 const user = mongoose.model('users', usersSchema);
